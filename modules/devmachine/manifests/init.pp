b1aefc1a01229732a976550d5ddaa92804f73b5e
Deline Neo <delineneo@gmail.com>
2015-08-16 05:41:09 -0700

    Set default ruby

--- modules/devmachine/manifests/init.pp
+++ modules/devmachine/manifests/init.pp
@@ -1,39 +1,48 @@
 class devmachine {
 
-	#Install Java 6
-	package { 'Legacy Java 6 JRE':
-	    ensure => installed,
-	    provider => appdmg,
-	    source => "http://delines-air.local:8000/javaforosx.dmg"
-  	}
+
+	# #Install Java 6
+	providerackage { 'Legacy Java 6 JRE':
+		ensure => installed,
+		provider => pkgdmg,
+		source => "http://delines-air.local:8000/javaforosx.dmg"
+	}
 
 	#Install Java 8
 
-	#install Sublime
+	#Install Sublime
 	package { 'Sublime':
-	    ensure => installed,
-	    provider => appdmg,
-	    source => "http://delines-air.local:8000/SublimeText2.0.2.dmg"
-  	}
+		ensure => installed,
+		provider => appdmg,
+		source => "http://delines-air.local:8000/SublimeText2.0.2.dmg"
+	}
 
-	#install IntelliJ
+	#Install IntelliJ
 	package { 'IntelliJ':
-	    ensure => installed,
-	    provider => appdmg,
-	    source => "http://delines-air.local:8000/ideaIC-14.1.4.dmg"
-  	}
+		ensure => installed,
+		provider => appdmg,
+		source => "http://delines-air.local:8000/ideaIC-14.1.4.dmg"
+	# }
 
 
 	#Install Xcode
 	#Install Xcode CLI
 	#install xctool
-	#install Cocoapods
-	#install Fastlane
 
-	#Install certificates
+	### Ruby ###
+	$ruby_version = "2.1.2"
 
-	#Notify complete
-	notify { 'hello':
-	  message  => "BLAH"
+	# Set global default ruby
+	class { 'ruby::global':
+  		version => $ruby_version
 	}
+
+	# # Install RubyGems - cocoapods, fastlane etc
+	ruby_gem { 'cocoapods':
+  		gem          => 'cocoapods',
+  		version      => '0.38.2',
+  		ruby_version => $ruby_version
+	}
+
+
 }
