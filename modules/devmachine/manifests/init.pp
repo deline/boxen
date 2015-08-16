dbc18950b6029c08a26634543acfc27123d8b906
Deline Neo <delineneo@gmail.com>
2015-08-15 23:23:39 -0700

    Add stuff needed for Java and iOS Dev

--- modules/devmachine/manifests/init.pp
+++ modules/devmachine/manifests/init.pp
@@ -1,4 +1,38 @@
 class devmachine {
+
+	#Install Java 6
+	package { 'Legacy Java 6 JRE':
+	    ensure => installed,
+	    provider => appdmg,
+	    source => "http://delines-air.local:8000/javaforosx.dmg"
+  	}
+
+	#Install Java 8
+
+	#install Sublime
+	package { 'Sublime':
+	    ensure => installed,
+	    provider => appdmg,
+	    source => "http://delines-air.local:8000/SublimeText2.0.2.dmg"
+  	}
+
+	#install IntelliJ
+	package { 'IntelliJ':
+	    ensure => installed,
+	    provider => appdmg,
+	    source => "http://delines-air.local:8000/ideaIC-14.1.4.dmg"
+  	}
+
+
+	#Install Xcode
+	#Install Xcode CLI
+	#install xctool
+	#install Cocoapods
+	#install Fastlane
+
+	#Install certificates
+
+	#Notify complete
 	notify { 'hello':
 	  message  => "BLAH"
 	}
